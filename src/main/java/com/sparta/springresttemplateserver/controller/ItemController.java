d5217cd5a4aa0eca366fe2cce7cacbc7333e6c8e
docoding0228 <npng0228@naver.com>
2024-05-27 17:20:53 +0900

    RestTemplate의 Get 요청

--- src/main/java/com/sparta/springresttemplateserver/controller/ItemController.java
+++ src/main/java/com/sparta/springresttemplateserver/controller/ItemController.java
@@ -12,25 +12,47 @@ public class ItemController {
 
     private final ItemService itemService;
 
+    // ItemService를 주입받아 초기화합니다.
     public ItemController(ItemService itemService) {
         this.itemService = itemService;
     }
 
+    /**
+     * 아이템 제목(query)을 받아 해당 아이템 객체를 반환합니다.
+     * @param query 검색할 아이템 제목
+     * @return 검색된 아이템 객체
+     */
     @GetMapping("/get-call-obj")
     public Item getCallObject(@RequestParam String query) {
         return itemService.getCallObject(query);
     }
 
+    /**
+     * 모든 아이템 리스트를 ItemResponseDto 객체로 반환합니다.
+     * @return ItemResponseDto 객체
+     */
     @GetMapping("/get-call-list")
     public ItemResponseDto getCallList() {
         return itemService.getCallList();
     }
 
+    /**
+     * POST 요청을 처리하여 아이템을 반환합니다.
+     * @param query 검색할 아이템 제목
+     * @param requestDto 사용자 요청 데이터
+     * @return 처리된 아이템 객체
+     */
     @PostMapping("/post-call/{query}")
     public Item postCall(@PathVariable String query, @RequestBody UserRequestDto requestDto) {
         return itemService.postCall(query, requestDto);
     }
 
+    /**
+     * 교환 요청을 처리하여 ItemResponseDto 객체를 반환합니다.
+     * @param token 인증 토큰
+     * @param requestDto 사용자 요청 데이터
+     * @return 처리된 ItemResponseDto 객체
+     */
     @PostMapping("/exchange-call")
     public ItemResponseDto exchangeCall(@RequestHeader("X-Authorization") String token, @RequestBody UserRequestDto requestDto) {
         return itemService.exchangeCall(token, requestDto);
--- src/main/java/com/sparta/springresttemplateserver/service/ItemService.java
+++ src/main/java/com/sparta/springresttemplateserver/service/ItemService.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Service
 public class ItemService {
 
+    // 초기 아이템 리스트를 설정합니다.
     private final List<Item> itemList = Arrays.asList(
             new Item("Mac", 3_888_000),
             new Item("iPad", 1_230_000),
@@ -19,18 +20,50 @@ public class ItemService {
             new Item("AirPods", 350_000)
     );
 
+    /**
+     * 아이템 제목(query)을 받아 해당 아이템 객체를 반환합니다.
+     * @param query 검색할 아이템 제목
+     * @return 검색된 아이템 객체 또는 null
+     * 하나의 아이템만 받아올때 사용.
+     */
     public Item getCallObject(String query) {
+        for (Item item : itemList) {
+            if(item.getTitle().equals(query)) {
+                return item;
+            }
+        }
         return null;
     }
 
+    /**
+     * 모든 아이템 리스트를 ItemResponseDto 객체로 반환합니다.
+     * @return ItemResponseDto 객체
+     * 여러개의 아이템을 받아올때 사용
+     */
     public ItemResponseDto getCallList() {
-        return null;
+        ItemResponseDto responseDto = new ItemResponseDto();
+        for (Item item : itemList) {
+            responseDto.setItems(item);
+        }
+        return responseDto;
     }
 
+    /**
+     * POST 요청을 처리하는 메소드입니다. 현재는 null을 반환합니다.
+     * @param query 검색할 아이템 제목
+     * @param requestDto 사용자 요청 데이터
+     * @return 현재는 null을 반환합니다.
+     */
     public Item postCall(String query, UserRequestDto requestDto) {
         return null;
     }
 
+    /**
+     * 교환 요청을 처리하는 메소드입니다. 현재는 null을 반환합니다.
+     * @param token 인증 토큰
+     * @param requestDto 사용자 요청 데이터
+     * @return 현재는 null을 반환합니다.
+     */
     public ItemResponseDto exchangeCall(String token, UserRequestDto requestDto) {
         return null;
     }
